f3b15bd058ade90f0d5c36a47bb79da0686c75b8
Petter Hannevold <petter.hannevold@nav.no>
2022-11-03 15:07:28 +0100

    La til dependency til logstash encoder

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,7 @@ repositories {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter:2.7.5")
     implementation("org.springframework.boot:spring-boot-starter-web:2.7.5")
+    implementation("net.logstash.logback:logstash-logback-encoder:7.2")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
 }
