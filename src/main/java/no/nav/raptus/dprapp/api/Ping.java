9f4fc81fceaeeb5d4e69d5fbccfc92dee0181c7f
Petter Hannevold <petter.hannevold@nav.no>
2022-11-03 13:18:07 +0100

    added test error endpoint (ping/error)

--- src/main/java/no/nav/raptus/dprapp/api/Ping.java
+++ src/main/java/no/nav/raptus/dprapp/api/Ping.java
@@ -1,5 +1,8 @@
 package no.nav.raptus.dprapp.api;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,8 +12,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("ping")
 public class Ping {
 
+    private static Logger logger = LoggerFactory.getLogger(Ping.class);
+
     @GetMapping
     public ResponseEntity<Void> ping() {
         return ResponseEntity.ok().build();
+    }
+
+    @GetMapping(path = "/error")
+    public ResponseEntity<String> error() {
+        logger.error("Test error");
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Test Error");
     }
 }
