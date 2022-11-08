317139f0fcca853ac6425531187f1528b2236065
Petter Hannevold <petter.hannevold@nav.no>
2022-11-08 14:58:18 +0100

    tok bort flyway for testing, la til manuell datasource

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web:2.7.5")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa:2.7.5")
     implementation("net.logstash.logback:logstash-logback-encoder:7.2")
-    implementation("org.flywaydb:flyway-core:9.4.0")
+//    implementation("org.flywaydb:flyway-core:9.4.0")
     implementation("org.yaml:snakeyaml:1.33")
     implementation("org.postgresql:postgresql:42.5.0")
     implementation("com.zaxxer:HikariCP:5.0.1")
--- src/main/java/no/nav/raptus/dprapp/Main.java
+++ src/main/java/no/nav/raptus/dprapp/Main.java
@@ -1,15 +1,45 @@
 package no.nav.raptus.dprapp;
 
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import no.nav.raptus.dprapp.api.Ping;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+import java.util.Properties;
 
 @Configuration
 @SpringBootApplication
 public class Main {
 
+    private static Logger logger = LoggerFactory.getLogger(Main.class);
+
     public static void main(String[] args) {
+        logger.info(System.getenv("DB_PASSWORD"));
+        logger.info(System.getenv("DB_USERNAME"));
         SpringApplication.run(Main.class, args);
+    }
+
+    @Bean
+    public DataSource dataSource() {
+
+        String url = "jdbc:postgresql://" + System.getenv("DB_HOST") +
+                ":" + System.getenv("DB_PORT") +
+                "/" + System.getenv("DB_DATABASE");
+
+
+        Properties properties = new Properties();
+        properties.put("driverClassName", "org.postgresql.Driver");
+        properties.put("username", System.getenv("DB_USERNAME"));
+        properties.put("password", System.getenv("DB_PASSWORD"));
+        properties.put("jdbcUrl", url);
+
+        return new HikariDataSource(new HikariConfig(properties));
     }
 }
