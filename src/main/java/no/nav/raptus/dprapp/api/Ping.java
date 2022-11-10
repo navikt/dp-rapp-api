ddeef1600c56752298f6378ea4bc45de347b7ff1
Petter Hannevold <petter.hannevold@nav.no>
2022-11-10 10:03:58 +0100

    Ryddet opp i midlertidig debug kode. La til mer kode for å debugge passord fra env-variabler

--- src/main/java/no/nav/raptus/dprapp/api/Ping.java
+++ src/main/java/no/nav/raptus/dprapp/api/Ping.java
@@ -2,33 +2,20 @@ package no.nav.raptus.dprapp.api;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.sql.DataSource;
-import java.sql.SQLException;
-
 @RestController
 @RequestMapping("ping")
 public class Ping {
 
-    @Autowired
-    public DataSource dataSource;
-
-    private static Logger logger = LoggerFactory.getLogger(Ping.class);
+    private static final Logger logger = LoggerFactory.getLogger(Ping.class);
 
     @GetMapping
     public ResponseEntity<String> ping() {
-        try {
-            dataSource.getConnection().commit();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
         return ResponseEntity.ok().build();
     }
 
