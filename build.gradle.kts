0c8c5beb614770d3247ca8d4f06a529ef4039f87
Mona Kjeldsrud <mona.kjeldsrud@nav.no>
2022-11-17 13:26:15 +0100

    Test for setting og fjerning av mdc-verdi

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter:2.7.5")
     implementation("org.springframework.boot:spring-boot-starter-web:2.7.5")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa:2.7.5")
+    implementation("org.springframework.boot:spring-boot-starter-test:2.7.5")
     implementation("net.logstash.logback:logstash-logback-encoder:7.2")
     implementation("org.flywaydb:flyway-core:9.7.0")
     implementation("org.yaml:snakeyaml:1.33")
@@ -21,8 +22,11 @@ dependencies {
     compileOnly("org.projectlombok:lombok:1.18.24")
     annotationProcessor("org.projectlombok:lombok:1.18.24")
 
+    // Test
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+//    testImplementation("org.assertj:assertj-core:3.23.1")
+//    testRuntimeOnly("org.assertj:assertj-core:3.23.1")
 }
 
 tasks.getByName<Test>("test") {
