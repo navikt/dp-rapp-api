b6a8ec355f9706ea7d2bb8b935f5a959220c2139
Igor Shuliakov <igor.shuliakov@nav.no>
2023-01-02 22:15:17 +0100

    API get

--- src/main/java/no/nav/raptus/dprapp/api/v1/SaveData.java
+++ src/main/java/no/nav/raptus/dprapp/api/v1/SaveData.java
@@ -1,12 +1,15 @@
 package no.nav.raptus.dprapp.api.v1;
 
 import lombok.extern.slf4j.Slf4j;
+import no.nav.raptus.dprapp.model.ActivityType;
 import no.nav.raptus.dprapp.model.Data;
+import no.nav.raptus.dprapp.model.Day;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @Slf4j
 @RestController
@@ -16,6 +19,45 @@ public class SaveData {
     @PostMapping(path = "/save")
     public ResponseEntity<String> save(@RequestBody Data data) {
         log.warn(data.toString());
+        // Sjekke at bruker i token "eier" innsendte meldekortet
+        // Lagre data
+
         return ResponseEntity.ok().build();
+    }
+
+    @PostMapping(path = "/send")
+    public ResponseEntity<String> send(@RequestBody Data data) {
+        // Lagre
+        save(data);
+        // Kontroller
+        // Svar
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/get/{id}")
+    public ResponseEntity<Data> get(@PathVariable String id) {
+        log.warn(id);
+
+        List<Day> days = new ArrayList<>();
+        days.add(Day.builder().date(LocalDate.of(2022, 12, 5)).type(ActivityType.WORK).hours(4d).build());
+        days.add(Day.builder().date(LocalDate.of(2022, 12, 6)).type(ActivityType.WORK).hours(3d).build());
+        days.add(Day.builder().date(LocalDate.of(2022, 12, 7)).type(ActivityType.MEASURES).build());
+        days.add(Day.builder().date(LocalDate.of(2022, 12, 8)).type(ActivityType.MEASURES).build());
+        days.add(Day.builder().date(LocalDate.of(2022, 12, 9)).type(ActivityType.ILLNESS).build());
+
+        days.add(Day.builder().date(LocalDate.of(2022, 12, 12)).type(ActivityType.ILLNESS).build());
+        days.add(Day.builder().date(LocalDate.of(2022, 12, 13)).type(ActivityType.MEASURES).build());
+        days.add(Day.builder().date(LocalDate.of(2022, 12, 14)).type(ActivityType.MEASURES).build());
+
+        Data data = new Data();
+        data.setId(id);
+        data.setQuestionWork(true);
+        data.setQuestionMeasures(true);
+        data.setQuestionIllness(true);
+        data.setQuestionVacation(false);
+        data.setDays(days);
+        data.setQuestionProceed(false);
+
+        return ResponseEntity.ok(data);
     }
 }
