dc7276413945a998f6cb5601afa29fad0a5efcc0
Igor Shuliakov <igor.shuliakov@nav.no>
2023-01-17 15:56:00 +0100

    Bigint i ID for mellomlagrede meldeperioder

--- src/main/resources/db/migration/V10__mellomlagret_meldeperiode.sql
+++ src/main/resources/db/migration/V10__mellomlagret_meldeperiode.sql
@@ -2,5 +2,16 @@
 -- Tabell            : mellomlgagret_meldeperiode
 -- Beskrivelse       : Kodeverkstabell som inneholder mellomlagrede meldeperioder
 -------------------------------------------------------------------------------
-DROP INDEX memepe_1;
+CREATE TABLE mellomlgagret_meldeperiode
+(
+    meldeperiode_id BIGINT PRIMARY KEY NOT NULL,
+    data            TEXT               NOT NULL
+);
 
+-- Indekser
+CREATE INDEX memepe_1 ON meldeperiode (meldeperiode_id);
+
+-- Tabell- og kolonnekommentarer
+COMMENT ON TABLE meldeperiode IS 'Kodeverkstabell som inneholder mellomlagrede meldeperioder.';
+COMMENT ON COLUMN meldeperiode.meldeperiode_id IS 'Primærnøkkel';
+COMMENT ON COLUMN meldeperiode.fom_dato IS 'JSON';
